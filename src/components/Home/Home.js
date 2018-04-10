6ebd799f9812f8afa7bcff7227c7cdcc87062256
Jordan Snow <snowstorm1989@gmail.com>
2018-04-10 16:17:16 -0700

    Test Routes and Create base components

---
+++ src/components/Home/Home.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+class Home extends Component {
+  render() {
+    return <div>This is the Home Page</div>;
+  }
+}
+export default Home;
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 export default (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/footer" component={Footer} />
   </Switch>
 );
