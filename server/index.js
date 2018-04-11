8bcbd5bea0ab25fe6fb5642744698fc5e3a79210
Jordan Snow <snowstorm1989@gmail.com>
2018-04-11 14:38:08 -0700

    Create ENV, Add  SASS,  Server changes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { json } = require("body-parser");
+const massive = require("massive");
 
 const port = 3005;
 const app = express();
+
+app.use(cors());
+app.use(json());
 
 app.listen(port, () => {
   console.log(`I am listening on port:${port}`);
